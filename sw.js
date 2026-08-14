f10ec113e4e486484697d2eac7b3ef50320b2571
kob <kobayashi@softdevice.co.jp>
2026-08-14 14:45:38 +0900

    red dot

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'radio-pad-v35';
-const ASSETS = ['./','./index.html','./styles.css?v=35','./app.js?v=35','./manifest.webmanifest','./icons/icon.svg'];
+const CACHE = 'radio-pad-v38';
+const ASSETS = ['./','./index.html','./styles.css?v=38','./app.js?v=38','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
